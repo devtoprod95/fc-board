002d4cf080fa20f5f4fd6ea55380b1317a6918e1
junyeob.kim <junyeob.kim@onch3.co.kr>
2025-10-14 17:18:42 +0900

    feat: Configure application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,8 +30,8 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
+    testImplementation("com.h2database:h2")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
@@ -58,4 +58,8 @@ tasks.withType<Test> {
 
 ktlint {
     ignoreFailures.set(true)
+}
+
+tasks.named<Jar>("jar") {
+    enabled = false
 }
