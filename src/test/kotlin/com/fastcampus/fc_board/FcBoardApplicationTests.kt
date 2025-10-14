eae6444adf5ea754347a46f4bb23baa2b02889d9
junyeob.kim <junyeob.kim@onch3.co.kr>
2025-10-14 15:46:14 +0900

    feat: Add first commit

---
+++ src/test/kotlin/com/fastcampus/fc_board/FcBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.fastcampus.fc_board
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
